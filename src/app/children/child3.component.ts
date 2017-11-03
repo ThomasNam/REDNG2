f4e05183a452eaf293a1d0c113ce3af038c14fa1
RED <purred@naver.com>
2017-11-04 00:48:01 +0900

    버그 수정

--- src/app/children/child3.component.ts
+++ src/app/children/child3.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'child3',
-    template: '<div>자식 3</div> <input type="text" [(ngModle)]="editName"> {{editName}}'
+    template: '<div>자식 3</div> <input type="text" [(ngModel)]="editName"> {{editName}}'
 })
 export class Child3Component 
 {
